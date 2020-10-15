b9b73c65bec3a1e050652822a374663a742b6013
Ikraam <ikraamghoor@gmail.com>
2020-10-15 20:02:20 +0200

    Add favourite a tech

--- src/pages/Tech.tsx
+++ src/pages/Tech.tsx
@@ -1,7 +1,7 @@
-import { IonCard, IonCardContent, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonLoading, IonPage, IonTitle, IonToolbar, useIonViewWillEnter } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonIcon, IonLoading, IonPage, IonTitle, IonToolbar, useIonViewWillEnter } from '@ionic/react';
+import { star, starOutline } from 'ionicons/icons';
 import React, { useState } from 'react';
 import { RootStateOrAny, useDispatch, useSelector } from 'react-redux';
-import ExploreContainer from '../components/ExploreContainer';
 import { request } from '../helpers/api';
 import { toast } from '../helpers/toast';
 import { setTeches } from '../redux/actions/dataActions';
@@ -12,10 +12,16 @@ const Tech: React.FC = () => {
   const dispatch = useDispatch()
   const [busy, setBusy] = useState<boolean>(false)
   const teches = useSelector((state: RootStateOrAny) => state.data.teches)
-  const store = useSelector((state: RootStateOrAny) => state)
-  // console.log("Tech:React.FC -> store", store)
-
   const {token} = user ? user : ''
+  // const store = useSelector((state: RootStateOrAny) => state)
+  // console.log("Tech:React.FC -> store", store)
+  const favourite = true
+
+  const handleFavourite = (tech_id:number) =>{
+  request(token, 'favourites', 'POST', {favourite:{tech_id}}).then((data) => console.log(data))
+  }
+
+
 
   useIonViewWillEnter(() =>{
      setBusy(true)
@@ -35,7 +41,7 @@ const Tech: React.FC = () => {
   const techItems = teches ? teches.map((tech:any) => {
     return ( 
       <IonCard key={tech.id}>
-        <img alt='mad' src="https://scontent-jnb1-1.xx.fbcdn.net/v/t31.0-8/1396939_377841179016599_1902197579_o.jpg?_nc_cat=104&_nc_sid=e3f864&_nc_ohc=MVdjGoPIrmAAX-_tsLQ&_nc_ht=scontent-jnb1-1.xx&oh=fa5ffbf10d32932f90b8b84955eea320&oe=5FAF305E" />
+        {/* <img alt='mad' src="https://scontent-jnb1-1.xx.fbcdn.net/v/t31.0-8/1396939_377841179016599_1902197579_o.jpg?_nc_cat=104&_nc_sid=e3f864&_nc_ohc=MVdjGoPIrmAAX-_tsLQ&_nc_ht=scontent-jnb1-1.xx&oh=fa5ffbf10d32932f90b8b84955eea320&oe=5FAF305E" /> */}
         <IonCardContent>
           <IonCardSubtitle>{tech.category}</IonCardSubtitle>
           <IonCardTitle>{tech.title}</IonCardTitle>
@@ -43,6 +49,7 @@ const Tech: React.FC = () => {
         <IonCardContent>
           {tech.description}
         </IonCardContent>
+      <IonButton onClick={ () => handleFavourite(tech.id)}> <IonIcon md={favourite ? star : starOutline} ></IonIcon></IonButton>
       </IonCard>)
   }) : ''
 

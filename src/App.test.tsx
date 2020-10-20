d33cf7c92c5edf9133a22cdd80f4daa148b3b659
Ikraam <ikraamghoor@gmail.com>
2020-10-20 02:40:28 +0200

    Fix tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
 
+import rootReducer from './redux/reducers/rootReducer';
+
+const store = createStore(rootReducer);
+
 test('renders without crashing', () => {
-  const { baseElement } = render(<App />);
+  const { baseElement } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
   expect(baseElement).toBeDefined();
 });

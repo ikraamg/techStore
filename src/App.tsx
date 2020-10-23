111095403ad063c5d2d8d808a742994a083632df
Ikraam <ikraamghoor@gmail.com>
2020-10-23 12:08:04 +0200

    Move home to containers

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
-import Home from './components/Home';
+import Home from './Containers/Home';
 import Favourites from './Containers/Favourites';
 import Tech from './Containers/Tech';
 
